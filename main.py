f8627c8cdc63766ae5ba64970b6377ac13f317f4
Perzival1312 <jj.mongoose11@hotmail.com>
2020-09-15 15:20:07 -0400

    psuedocode

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
 '''
+-------------------------
+
 Classic Snake Games made in Python
 
+-------------------------
+
+input:
+    Keyboard controls
+
+output:
+    fancy visuals
+
+-------------------------
+
+Initial:
+    display grid
+    single square snake start in the center of board
+    apple appear in a random location
+
+Starting:
+    Snake
+        moving
+        controllable
+            WASD or Arrow keys
+    Apple
+        disappear when eaten
+        reappear in a different random location
+
+continuation:
+    snake grows after eating an apple
+    remember location of turns so the body follows the path of the head
+    fancier visuals
+
+Epilogue:
+    "evil" apples - kill upon consumption
+
+-------------------------
 '''
